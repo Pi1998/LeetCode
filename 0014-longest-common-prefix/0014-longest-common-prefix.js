0bee9cfb36d953dddd491d2b2eec3fb8acd945ab
Shinn Thant Swam Ye <110252746+Pi1998@users.noreply.github.com>
2024-11-07 23:56:01 +0630

    Time: 0 ms (100%), Space: 48.9 MB (82.81%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+
+    strs.sort((a,b) => a.length - b.length)
+    let prefix = strs.shift()
+
+    for(let i=0; i < strs.length; i++){
+        while (strs[i].indexOf(prefix) != 0){
+            prefix = prefix.substring(0, prefix.length - 1);
+            if(prefix === "") return "";
+        }
+    }
+    return prefix
+};
